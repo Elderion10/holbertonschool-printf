ae95c4b3ab52e5fb4ad2f5a7ee8c20707b97b9aa
Elderion10 <yahurifrancisco10@gmail.com>
2024-07-17 14:00:51 -0400

    Se concluyen las pruebas del task 0 de momento

---
+++ test.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+int _printf(const char *format, ...);
+/**
+ * _printf - blabla
+ * @format: pointer
+ * Return: the number of characters
+ */
+int _printf(const char *format, ...)
+{
+	const char *a;
+	va_list lista;
+	int sum;
+	char *s;
+	unsigned i;
+
+	sum = 0;
+
+	if (!format)
+	{
+		return (-1);
+	}
+
+	va_start(lista, format);
+	for (a = format; *a != '\0'; a++)
+	{
+		if (s = va_arg(lista, char *))
+				puts(s);
+
+		if ("%c")
+			_putchar(i);
+
+		if ("%")
+			_putchar('%');
+
+		sum += va_arg(lista, int);
+	}
+
+	va_end(lista);
+	return (sum);
+}
+
+
+void main(void)
+{
+	char *b;
+	char nombre;
+	nombre = 'Z';
+	b = "soy yo";
+
+	printf("Hola %s, %c\n", b, nombre);
+	
+}
