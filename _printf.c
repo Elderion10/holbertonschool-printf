f64ebd86bd0c75802b567a94af3f766c86683dfa
Elderion10 <yahurifrancisco10@gmail.com>
2024-07-19 21:08:28 -0400

    se borro un tab de cada linea de codigo para que cumpliera con los requerimientos de betty

--- _printf.c
+++ _printf.c
@@ -17,35 +17,35 @@ int _printf(const char *format, ...)
 	sum = 0;
 	va_start(argumentos, format);
 
-	if (format == NULL || !format[i + 1])
-		return (-1);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
+		if (format == NULL || !format[i + 1])
+			return (-1);
+		for (i = 0; format[i] != '\0'; i++)
 		{
-			if (format[i + 1])
+			if (format[i] == '%')
 			{
-				if (format[i + 1] != 'c' && format[i + 1] != 's'
-						&& format[i + 1] != '%')
+				if (format[i + 1])
 				{
-					sum += _putchar(format[i]);
-				}
-				else
-				{
-					if (f == NULL)
-						_putchar(format[i]);
-					f = get_func(&format[i + 1]);
-					sum += f(argumentos);
-					i++;
+					if (format[i + 1] != 'c' && format[i + 1] != 's'
+							&& format[i + 1] != '%')
+					{
+						sum += _putchar(format[i]);
+					}
+					else
+					{
+						if (f == NULL)
+							_putchar(format[i]);
+						f = get_func(&format[i + 1]);
+						sum += f(argumentos);
+						i++;
+					}
 				}
 			}
+			else
+			{
+				_putchar(format[i]);
+				sum++;
+			}
 		}
-		else
-		{
-			_putchar(format[i]);
-			sum++;
-		}
-	}
-	va_end(argumentos);
-	return (sum);
+		va_end(argumentos);
+		return (sum);
 }
